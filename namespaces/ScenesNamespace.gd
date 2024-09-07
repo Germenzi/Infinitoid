b847b7eff5e36b607b4775d818fe1310d1fc8ef8
Nikita\Nick <kineeetik@gmail.com>
2024-09-07 21:10:18 +0300

    Add tutorial

--- namespaces/ScenesNamespace.gd
+++ namespaces/ScenesNamespace.gd
@@ -5,3 +5,4 @@ class_name ScenesNamespace
 const MAIN_MENU_SCENE_PATH : String = "res://scenes/main.tscn"
 const GAME_SCENE_PATH : String = "res://scenes/arcanoid_game.tscn"
 const LEVEL_SELECTING_SCENE_PATH : String = "res://scenes/level_selecting.tscn"
+const TUTORIAL_SCENE_PATH : String = "res://scenes/tutorial.tscn"
