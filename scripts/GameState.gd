4a8780c7189968a199d5483dfc2e5a8207d7ff5b
Nikita\Nick <kineeetik@gmail.com>
2026-04-30 21:08:08 +0300

    add progression

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -20,9 +20,15 @@ var ball_position : Vector2
 
 var blocks : Array[ArcanoidBlock] = []
 var blocks_spawn_count : int
+var block_hp_spawn_rate : Dictionary[int, float] = {}
 
 var health : int
-var score : int
+var score : int :
+	set(v):
+		score = v
+		_on_score_changed()
+	get:
+		return score
 
 
 func load_level_config(level_config:LevelConfig) -> void:
@@ -37,7 +43,8 @@ func load_level_config(level_config:LevelConfig) -> void:
 			(GlobalSettings.game_config.block_size + Vector2(GlobalSettings.game_config.blocks_columns_gap, 0)) 
 				* Vector2(randi() % GlobalSettings.game_config.block_column_count, 0), 
 		GlobalSettings.game_config.block_size), 1))
-
+	
+	block_hp_spawn_rate[1] = 1.0
 
 
 func _enter_tree() -> void:
@@ -45,3 +52,42 @@ func _enter_tree() -> void:
 	platform_rect = ArcanoidRect.new(Rect2(Vector2.ZERO, GlobalSettings.game_config.initial_platform_size))
 	health = GlobalSettings.game_config.maximum_health
 	score = 0
+
+
+func _on_score_changed() -> void:
+	const NEW_BLOCKS_BEIGN_AT_SCORE : int = 10
+	const ADD_ANOTHER_SPAWN_BLOCK_SCORE_RATE : int = 30
+	
+	var need_new_block : bool = (score - NEW_BLOCKS_BEIGN_AT_SCORE) % ADD_ANOTHER_SPAWN_BLOCK_SCORE_RATE == 0
+	if need_new_block:
+		blocks_spawn_count = min(blocks_spawn_count + 1, GlobalSettings.game_config.block_column_count)
+	
+	const NEW_HP_BEGIN_AT_SCORE : int = 20
+	const NEW_HP_SCORE_RATE : int = 30
+	
+	var need_new_hp : bool = (score - NEW_HP_BEGIN_AT_SCORE) % NEW_HP_BEGIN_AT_SCORE == 0
+	if need_new_hp:
+		var step : int =  (score - NEW_HP_BEGIN_AT_SCORE) / NEW_HP_BEGIN_AT_SCORE + 1
+		if step > 4:
+			return
+		
+		block_hp_spawn_rate = {}
+		match step:
+			1:
+				block_hp_spawn_rate[1] = 0.8
+				block_hp_spawn_rate[2] = 0.2
+			2:
+				block_hp_spawn_rate[1] = 0.7
+				block_hp_spawn_rate[2] = 0.2
+				block_hp_spawn_rate[3] = 0.1
+			3:
+				block_hp_spawn_rate[1] = 0.6
+				block_hp_spawn_rate[2] = 0.2
+				block_hp_spawn_rate[3] = 0.15
+				block_hp_spawn_rate[4] = 0.05
+			4:
+				block_hp_spawn_rate[1] = 0.4
+				block_hp_spawn_rate[2] = 0.3
+				block_hp_spawn_rate[3] = 0.15
+				block_hp_spawn_rate[4] = 0.1
+				block_hp_spawn_rate[5] = 0.05
