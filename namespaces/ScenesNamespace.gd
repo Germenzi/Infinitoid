f6cdc8125a842ff6ff8f0c78129b9b5ff06f7c26
Nikita\Nick <kineeetik@gmail.com>
2026-04-30 19:58:49 +0300

    removed tutorial

--- namespaces/ScenesNamespace.gd
+++ namespaces/ScenesNamespace.gd
@@ -4,4 +4,3 @@ class_name ScenesNamespace
 
 const MAIN_MENU_SCENE_PATH : String = "res://scenes/main.tscn"
 const GAME_SCENE_PATH : String = "res://scenes/arcanoid_game.tscn"
-const TUTORIAL_SCENE_PATH : String = "res://scenes/tutorial.tscn"
