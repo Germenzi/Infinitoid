bbec963873354a8275353bd41c0129a97f67806f
Nikita\Nick <kineeetik@gmail.com>
2026-05-01 16:41:43 +0300

    add YandexSDK (currently doesn't work)

--- scripts/GlobalSettingsAutoload.gd
+++ scripts/GlobalSettingsAutoload.gd
@@ -2,9 +2,15 @@ extends Node
 
 signal game_scale_changed
 
+var game_over_count : int = 0
+
 var game_scale : float = 1.0 :
 	set(v):
 		game_scale = v
 		game_scale_changed.emit()
 
 var game_config : GameConfig = preload("res://resources/main_game_config.tres")
+
+
+func _ready() -> void:
+	YandexSDK.init_game()
