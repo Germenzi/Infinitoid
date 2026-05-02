81b42d22de1d341aa9c586e77df6f70c9d6396eb
Nikita\Nick <kineeetik@gmail.com>
2026-05-02 19:11:26 +0300

    huasebe

--- scripts/GlobalSettingsAutoload.gd
+++ scripts/GlobalSettingsAutoload.gd
@@ -8,3 +8,14 @@ var game_scale : float = 1.0 :
 		game_scale_changed.emit()
 
 var game_config : GameConfig = preload("res://resources/main_game_config.tres")
+
+var games_played : int = 0
+
+var lang :String
+
+func _ready() -> void:
+	if !WebBus.is_init:
+		await WebBus.inited
+	
+	WebBus.ready()
+	lang = WebBus.get_language()
